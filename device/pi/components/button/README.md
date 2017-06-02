579e4581920bc1565ce375ce2e6b8a1ff01d3eec
Stefan Hassferter <stefan.hassferter@crossbario.com>
2017-06-02 12:54:16 +0200

    files upload

--- device/pi/components/button/README.md
+++ device/pi/components/button/README.md
@@ -1,1 +1,106 @@
-Write me ...
+# Crossbar.io IoT Starterkit - Button and LED
+
+## Overview
+
+This component wramps a Button and a LED connected to the Rasperry Pis GPIO-Pins. The Button can be used for input and the LED can show warnings or just notifies the user about a special programm state.
+
+The component is written in Python using Autobahn|Python running on Twisted. The [Dockerfile](Dockerfile) for the component uses the default `crossbario/autobahn-python-armhf` image as base.
+
+
+## How to run
+
+Run the buzzer component on the Pi following **[this procedure](https://github.com/crossbario/iotcookbook/tree/master/device/pi/components#how-to-run)**:
+
+```console
+cd iotcookbook/device/pi/component/button
+make start
+```
+
+
+(and it may take a while on first start as the necessary Docker image is downloaded).
+
+Then open this URL:
+
+* [https://demo.crossbar.io/iotcookbook/device/pi/recipes/buzzer](https://demo.crossbar.io/iotcookbook/device/pi/recipes/buzzer)
+
+in your browser.
+
+The control page will ask you for the serial number of your Pi. This is being put out as part of the component startup logging, or you can do `grep Serial /proc/cpuinfo` and drop any leading zeros.
+
+> Alternatively, you can construct the URL for direct access by adding '?serial=41f4b2fb' to its end, where you replace '41f4b2fb' with the serial of your Pi.
+
+You should then see a Web page with buttons to control the piezo buzzer on your Pi. Pressing a button on the Web page will issue a WAMP remote procedure call to the `beep()` procedure exposed by the buzzer component running on the Pi.
+
+This demonstrates secure remote procedure calls from any browser based device to an embedded device running a Python/Docker component and possibly behind firewalls and NATs.
+
+> The buttons ("caller") and buzzers ("callees") of course can be required to be authenticated as well as authorized in production.
+
+
+## API
+
+The component uses an URI prefix containing the Pi serial number
+
+* `io.crossbar.demo.iotstarterkit.<serial>.buzzer`
+
+eg the Pi with serial no. `41f4b2fb` will use URIs starting with
+
+* `io.crossbar.demo.iotstarterkit.41f4b2fb.buzzer`
+
+
+### Procedures
+
+#### beep
+
+To trigger a beeping sound sequence, call
+
+* `io.crossbar.demo.iotstarterkit.<serial>.buzzer.beep(count, on, off)`
+
+with (positional) parameters
+
+* `count`: Number of beeps, default `1`.
+* `on`: ON duration in ms, default `30`.
+* `off`: OFF duration in ms, default `80`.
+
+#### is_beeping
+
+To check whether the buzzer is currently beeping, call
+
+* `io.crossbar.demo.iotstarterkit.<serial>.buzzer.is_beeping()`
+
+The procedure takes no parameters and returns a single positional result with a boolean flag.
+
+#### welcome
+
+To play a whole welcome beeping sequence, call
+
+* `io.crossbar.demo.iotstarterkit.<serial>.buzzer.welcome()`
+
+The procedure takes no parameters.
+
+
+### Events
+
+#### on_beep_started
+
+The component will emit an event
+
+* `io.crossbar.demo.iotstarterkit.<serial>.buzzer.on_beep_started(..)`
+
+with keyword-based parameters:
+
+* `count`: Number of beeps in the started sequence.
+* `on`: ON duration in ms in the started sequence.
+* `off`: OFF duration in ms in the started sequence.
+
+#### on_beep_ended
+
+When the current beeping sequence is finished, the component will emit en event
+
+* `io.crossbar.demo.iotstarterkit.<serial>.buzzer.on_beep_ended()`
+
+
+### Errors
+
+When a beeping sequence is currently playing, calling `beep()` will raise an error:
+
+* `io.crossbar.demo.iotstarterkit.<serial>.buzzer.already-beeping()`
---
+++ device/pi/components/button/app/client.py
@@ -0,0 +1,239 @@
+#!/usr/bin/python
+
+from threading import current_thread
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks
+from twisted.internet.error import ReactorNotRunning
+
+from autobahn.util import utcnow
+from autobahn.twisted.util import sleep
+from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
+from autobahn.wamp.exception import ApplicationError
+
+import RPi.GPIO as GPIO
+
+
+import os
+import argparse
+
+import six
+
+import txaio
+txaio.use_twisted()
+
+
+def get_serial():
+    """
+    Get the Pi's serial number.
+    """
+    with open('/proc/cpuinfo') as fd:
+        for line in fd.read().splitlines():
+            line = line.strip()
+            if line.startswith('Serial'):
+                _, serial = line.split(':')
+                return serial.strip().lstrip('0')
+
+
+def config_button_gpio(channel):
+    """
+    configure the Button GPIO Pin
+    """
+
+    GPIO.setmode(GPIO.BCM)
+
+    # declare GPIO Pin as input and enable the pull-up resistor
+    GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+def config_LED_gpio(LED_pin):
+
+    """
+    configure the LED - GPIO Pin
+    """
+    GPIO.setmode(GPIO.BCM)
+
+    # declare GPIO Pin as output
+    GPIO.setup(LED_pin, GPIO.OUT)
+
+
+class ButtonComponent(ApplicationSession):
+    """Our component wrapping a Button with edge detection."""
+
+    LED_status = False
+
+    @inlineCallbacks
+    def onJoin(self, details):
+        self.log.info('Session joined on thread {thread_id}: {details}', thread_id=current_thread().ident, details=details)
+
+        LED_pin = 15
+
+        self.LED_status = False
+        """Callback when the WAMP session has been established and is ready for use."""
+        # get the Pi serial number
+        self._serial = get_serial()
+
+        # all procedures/events will have this URI prefix
+        self._prefix = u'io.crossbar.demo.iotstarterkit.{}.button'.format(self._serial)
+
+        # print startup infos
+        self.log.info("Crossbar.io IoT Starterkit Serial No.: {serial}", serial=self._serial)
+        self.log.info("ButtonComponent connected: {details}", details=details)
+
+        # get component user extra configuration
+        cfg = self.config.extra
+
+        # initialize button
+        self._button_pin = cfg['button_pin']
+        GPIO.setwarnings(False)
+        # config_button_gpio(_button_pin)
+        config_button_gpio(18)
+        GPIO.add_event_detect(18, GPIO.FALLING, callback=self.press, bouncetime=250)
+
+        # initialize LED
+        config_LED_gpio(LED_pin)
+
+        # remember startup timestamp
+        self._started = utcnow()
+
+        # flag indicating if the button is already pressed
+        self._is_pressed = False
+
+        # register procedures
+        for proc in [
+            (self.started, u'started'),
+            (self._is_pressed, u'is_pressed'),
+            (self.press, u'press'),
+            (self.led_on, u'led_on'),
+            (self.led_off, u'led_off')
+        ]:
+            uri = u'{}.{}'.format(self._prefix, proc[1])
+            yield self.register(proc[0], uri)
+            self.log.info('registered procedure {uri}', uri=uri)
+
+        self._is_ready = True
+        self.log.info("ButtonComponent ready!")
+
+    def started(self):
+        """
+        Get UTC timestamp when the component started.
+
+        :returns: ISO8601 formatted UTC timestamp when the component started.
+        :rtype: str
+        """
+        return self._started
+
+    def is_pressed(self):
+        """
+        Check if the buzzer is currently playing a beeping sequence, and hence a (further)
+        concurrent call to beep() will fail.
+
+        :returns: Flag indicating whether the buzzer is currently beeping.
+        :rtype: bool
+        """
+        return self._is_pressed
+
+    def get_status(self):
+
+        return self.LED_status
+
+    def set_status(self, status):
+
+        self.LED_status = status
+        # self.log.info(self.get_status())
+        return 0
+
+    def press(self, portnr=1000):
+        """is called by the edge detection and running in a new thread
+            calls the _press() method which performs the action triggered by the button """
+        self.log.info('GPIO edge callback on thread {thread_id}', thread_id=current_thread().ident)
+        reactor.callFromThread(self._press)
+
+    @inlineCallbacks
+    def _press(self):
+        """button pressed handler"""
+
+        self.log.info('Button pressed handler on thread {thread_id}', thread_id=current_thread().ident)
+
+        """ if the button is pressed during the progress is running this Error will be fired"""
+        if self._is_pressed:
+            raise ApplicationError(u'{}.already-pressed'.format(self._prefix), 'Button is already pressed ')
+
+        self._is_pressed = True
+        self.log.info("Pressed")
+
+        """ publish event button_pressed"""
+        self.publish(u'{}.button_pressed'.format(self._prefix))
+
+        """wait for a short time"""
+        yield sleep(1000 / 1000.)
+
+        self._is_pressed = False
+        """ publish event button_released"""
+        self.publish(u'{}.button_released'.format(self._prefix))
+
+        self.log.info("released")
+
+    @inlineCallbacks
+    def led_on(self, LED_pin=15):
+        """ callback function to put LED GPIO high => LED on"""
+        if self.get_status() is False:
+            GPIO.output(LED_pin, GPIO.HIGH)
+            self.publish(u'{}.LED_on'.format(self._prefix))
+            self.log.info("LED_on")
+            self.set_status(True)
+
+        yield sleep(1 / 1000.)
+
+    @inlineCallbacks
+    def led_off(self, LED_pin=15):
+        """ callback function to put LED GPIO low => LED off """
+        if self.get_status() is True:
+            GPIO.output(LED_pin, GPIO.LOW)
+            self.publish(u'{}.LED_off'.format(self._prefix))
+            self.log.info("LED_off")
+            self.set_status(False)
+
+        yield sleep(1 / 1000.)
+
+    def onLeave(self, details):
+        self.log.info("session closed: {details}", details=details)
+        self.disconnect()
+        GPIO.output(15, GPIO.LOW)
+        GPIO.cleanup()
+
+    def onDisconnect(self):
+        self.log.info("connection closed")
+        try:
+            reactor.stop()
+        except ReactorNotRunning:
+            pass
+
+
+if __name__ == '__main__':
+
+    # Crossbar.io connection configuration
+    url = os.environ.get('CBURL', u'wss://demo.crossbar.io/ws')
+    realm = os.environ.get('CBREALM', u'crossbardemo')
+
+    # parse command line parameters
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true', help='Enable debug output.')
+    parser.add_argument('--url', dest='url', type=six.text_type, default=url, help='The router URL (default: "ws://localhost:8080/ws").')
+    parser.add_argument('--realm', dest='realm', type=six.text_type, default=realm, help='The realm to join (default: "realm1").')
+
+    args = parser.parse_args()
+
+    if args.debug:
+        txaio.start_logging(level='debug')
+    else:
+        txaio.start_logging(level='info')
+
+    # custom configuration data
+    extra = {
+        # GPI pin of buzzer
+        u'button_pin': 18,
+    }
+
+    # create and start app runner for our app component ..
+    runner = ApplicationRunner(url=args.url, realm=args.realm, extra=extra)
+    runner.run(ButtonComponent, auto_reconnect=True)
